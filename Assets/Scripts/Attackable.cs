5c06c4a40834294362a176948f75517f34a50ddc
Mitch T <mtexel13@unl.edu>
2023-02-20 16:43:43 -0600

    Added ball Attackable script

---
+++ Assets/Scripts/Attackable.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attackable : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Attackable"))
+        {
+            //other.gameObject.SetActive(false);
+
+            other.gameObject.transform.position = new Vector3(0, 5, 0);
+            //AudioSource.PlayClipAtPoint(sliceSound, transform.position);
+        } else
+        {
+            other.gameObject.SetActive(true);
+        }
+    }
+
+
+}
